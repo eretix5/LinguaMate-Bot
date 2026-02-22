00aab3e77aa6ad00911a14c80bcbbc72a4061db8
Marat <mseitovich@mail.ru>
2026-02-23 02:05:28 +0300

    adding questions as collection

---
+++ src/questions.py
@@ -0,0 +1,52 @@
+questions = [
+    {
+        'question': 'Как читается ひらがな "あ"?',
+        'options': ['a', 'i', 'u', 'e'],
+        'correct_answer': 'a'
+    },
+    {
+        'question': 'Как читается ひらがな "か"?',
+        'options': ['ka', 'ki', 'ko', 'ke'],
+        'correct_answer': 'ka'
+    },
+    {
+        'question': 'Как переводится слово ねこ (neko)?',
+        'options': ['собака', 'кошка', 'птица', 'рыба'],
+        'correct_answer': 'кошка'
+    },
+    {
+        'question': 'Как переводится слово みず (mizu)?',
+        'options': ['вода', 'огонь', 'земля', 'воздух'],
+        'correct_answer': 'вода'
+    },
+    {
+        'question': 'Как читается ひらがな "さ"?',
+        'options': ['sa', 'shi', 'su', 'so'],
+        'correct_answer': 'sa'
+    },
+    {
+        'question': 'Как переводится слово ありがとう?',
+        'options': ['привет', 'пока', 'спасибо', 'извини'],
+        'correct_answer': 'спасибо'
+    },
+    {
+        'question': 'Как читается ひらがな "た"?',
+        'options': ['ta', 'chi', 'tsu', 'to'],
+        'correct_answer': 'ta'
+    },
+    {
+        'question': 'Как переводится слово いぬ (inu)?',
+        'options': ['кошка', 'собака', 'лошадь', 'тигр'],
+        'correct_answer': 'собака'
+    },
+    {
+        'question': 'Как читается ひらがな "な"?',
+        'options': ['na', 'ni', 'nu', 'ne'],
+        'correct_answer': 'na'
+    },
+    {
+        'question': 'Как переводится слово ほん (hon)?',
+        'options': ['книга', 'ручка', 'дом', 'машина'],
+        'correct_answer': 'книга'
+    }
+]
